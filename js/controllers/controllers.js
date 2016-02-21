a135af8d8b22f0db62044194386f6132e4d9925c
Shane-Lester <shane_lester@hotmail.com>
2016-02-21 12:08:53 +0000

    Completed add a section functionality

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -36,16 +36,6 @@ angular.module('JSONApp')
 		Items.remove(index);
 	}
 
-	$scope.moveUp = function(index){
-		//if second argument -1 move it up in the array
-		Items.move(index,-1);
-	}
-
-	$scope.moveDown = function(index){
-		//if second argument +1 move it down in the array
-		Items.move(index,+1);
-	}
-
 }])
 
 //edit or create an item for JSON
@@ -70,52 +60,8 @@ angular.module('JSONApp')
 		//let's create a deep copied duplicate of the array only to work on
 		$scope.copy = [];
 		$scope.copy=_.clone( $scope.object.data);
-		// if the data array is not empty, read the elements and apply them to the scope of the form to edit them
-		// if($scope.object.data.length >0){
-
-		// 	if($scope.object.data[0].heading){
-		// 		$scope.item.heading = $scope.object.data[0].heading;
-		// 	}
-		// 	if($scope.object.data[1].subheading){
-		// 		$scope.item.subheading = $scope.object.data[1].subheading;
-		// 	}
-		// 	if($scope.object.data[2].info1){
-		// 		$scope.item.info1 = $scope.object.data[2].info1;
-		// 		}
-		// 	if($scope.object.data[3].info2){
-		// 	$scope.item.info2 = $scope.object.data[3].info2;
-		// 		}
-		// }
-		// $scope.editting = $scope.object;
 	}
-	// console.log($scope.editting.title);
 	$scope.save = function(){
-
-
-		// $scope.editting.data =[]; 
-		// //only push the data items if they are there
-		// //otherwise push a dash- to keep it consistent
-		// if(!$scope.item.heading){
-		// 	$scope.item.heading = "-";
-		// 	}
-
-		// if(!$scope.item.subheading){
-		// 	$scope.item.subheading = "-";
-		// 	}
-		// if(!$scope.item.info1){
-		// 	$scope.item.info1 = "-";
-		// 	}
-		// if(!$scope.item.info2){
-		// 	$scope.item.info2 = "-";
-		// 	}
-		// $scope.editting.data.push({heading:$scope.item.heading});
-		// $scope.editting.data.push({subheading:$scope.item.subheading});
-		// $scope.editting.data.push({info1:$scope.item.info1});
-		// $scope.editting.data.push({info2:$scope.item.info2});
-
-		// // console.log($scope.editting);
-
-		// Items.save($scope.editting, $scope.id);
 		$scope.copy.forEach(function(item){
 
 			for(key in item){
@@ -127,6 +73,15 @@ angular.module('JSONApp')
 		angular.copy($scope.copy,$scope.object.data);
 
 		$state.go('app.home');
+	}
+
+	$scope.addSection = function(){
+
+		$scope.copy.push({"heading":""});
+		$scope.copy.push({"subheading":""});
+		$scope.copy.push({"info1":""});
+		$scope.copy.push({"info2":""});
+		$scope.copy.push({"item":"<hr>"});
 	}
 
 }])
